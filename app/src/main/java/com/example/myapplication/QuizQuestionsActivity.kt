de01fdefc23d5b41ae5be23a0cc8106d3467db61
Thomas Compton IV <tacompton4@gmail.com>
2022-01-25 23:35:28 -0500

    Now asks Question to player

--- app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/myapplication/QuizQuestionsActivity.kt
@@ -3,13 +3,42 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuizQuestionsActivity : AppCompatActivity() {
+    //Variables
+    private var progressBar : ProgressBar? = null
+    private var progressText : TextView? = null
+    private var theQuestion : TextView? = null
+    private var flagImage : ImageView? = null
+
+    private var optionOne : TextView? = null
+    private var optionTwo : TextView? = null
+    private var optionThree : TextView? = null
+    private var optionFour : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
 
+        progressBar = findViewById(R.id.progressBar)
+        progressText = findViewById(R.id.progressText)
+        theQuestion = findViewById(R.id.theQuestion)
+        flagImage = findViewById(R.id.flagImage)
+
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
+
+        for(i in questionsList){
+            Log.e("Questions", i.question)
+        }
+
+        var currentPosition = 1
+        val question : Question = questionsList[currentPosition - 1]
+        progressBar?.progress = currentPosition
+        progressText?.text = "$currentPosition / ${progressBar?.max}"
+        theQuestion?.text = question.question
     }
 }
